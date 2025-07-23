e487f3347b060369da7b718b2808674a7fa09477
luthoregarcon <mxllywd@gmail.com>
2025-07-23 11:12:06 +0300

    updated quadcore lib

--- quadcore/src/lib.rs
+++ quadcore/src/lib.rs
@@ -1,14 +1,15 @@
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
-#[pymodule]
-fn quadcore(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn recalculate_mesh(py: Python, mesh: PyObject) -> PyResult<()> {
+    println!("Called recalculate_mesh (stub)");
     Ok(())
 }
+
+#[pymodule]
+fn rustlib(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(recalculate_mesh, m)?)?;
+    Ok(())
+}
+
+
