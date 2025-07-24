8089dea07f83b72eacad243e03f0d5b73399290d
luthoregarcon <mxllywd@gmail.com>
2025-07-24 12:21:42 +0300

    updated quad algorithm

--- quadcore/src/lib.rs
+++ quadcore/src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 
 #[pyfunction]
 fn recalculate_quads(
@@ -7,28 +8,31 @@ fn recalculate_quads(
 ) -> PyResult<(Vec<(f32, f32, f32)>, Vec<Vec<usize>>)> {
     println!("Recalculating quads...");
 
-    let mut new_faces = Vec::new();
+    let mut new_faces: Vec<Vec<usize>> = Vec::new();
 
     for face in faces {
-        match face.len() {
-            4 => {
-                new_faces.push(face);
-            }
-            3 => {
-                let mut quad = face.clone();
-                quad.push(quad[2]);
-                new_faces.push(quad);
-            }
-            n if n > 4 => {
-                for i in 1..(face.len() - 2) {
-                    let quad = vec![face[0], face[i], face[i + 1], face[i + 2]];
-                    new_faces.push(quad);
-                }
-            }
-            _ => {
+        if face.len() < 3 {
+            continue;
+        }
+
+        let mut chunk = Vec::new();
+        for idx in face {
+            chunk.push(idx);
+            if chunk.len() == 4 {
+                new_faces.push(chunk.clone());
+                chunk.clear();
             }
         }
+
+        if !chunk.is_empty() {
+            while chunk.len() < 4 {
+                let last = *chunk.last().unwrap();
+                chunk.push(last);
+            }
+            new_faces.push(chunk);
+        }
     }
+
     Ok((vertices, new_faces))
 }
 
@@ -37,5 +41,4 @@ fn quadcore(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(recalculate_quads, m)?)?;
     Ok(())
 }
-
 
