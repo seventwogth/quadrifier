cd00eace983924eb75713f6a9ef026cbea93793f
luthoregarcon <mxllywd@gmail.com>
2025-07-24 11:00:58 +0300

    updated quadcore | first greedy quad algorithm iteration

--- quadcore/src/lib.rs
+++ quadcore/src/lib.rs
@@ -1,13 +1,36 @@
+
 use pyo3::prelude::*;
-use pyo3::types::PyTuple;
 
 #[pyfunction]
 fn recalculate_quads(
     vertices: Vec<(f32, f32, f32)>,
     faces: Vec<Vec<usize>>,
 ) -> PyResult<(Vec<(f32, f32, f32)>, Vec<Vec<usize>>)> {
-    println!("Called recalculate_quads (stub)");
-    Ok((vertices, faces))
+    println!("Recalculating quads...");
+
+    let mut new_faces = Vec::new();
+
+    for face in faces {
+        match face.len() {
+            4 => {
+                new_faces.push(face);
+            }
+            3 => {
+                let mut quad = face.clone();
+                quad.push(quad[2]);
+                new_faces.push(quad);
+            }
+            n if n > 4 => {
+                for i in 1..(face.len() - 2) {
+                    let quad = vec![face[0], face[i], face[i + 1], face[i + 2]];
+                    new_faces.push(quad);
+                }
+            }
+            _ => {
+            }
+        }
+    }
+    Ok((vertices, new_faces))
 }
 
 #[pymodule]
@@ -15,4 +38,5 @@ fn quadcore(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(recalculate_quads, m)?)?;
     Ok(())
 }
+
 
